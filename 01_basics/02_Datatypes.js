2fad716be0f1227ea8548079ddb5a038d4b0a2e4
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-03 12:15:43 +0530

    Create 02_Datatypes.js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+/*
+int
+BigInt
+String
+Object
+undefined
+null
+symbol=>scope
+boolean
+*/
+
+// null is an object
+let id =1234
+let name = "Vishnu"
+let aj
+
+console.table([typeof(id),typeof(name),typeof(aj),typeof(null)])
