14f132ead9da31edca5e6897faab4398b2b30d10
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-03 12:17:10 +0530

    Create DataConversion.js
    Data Type Conversions

---
+++ 01_basics/DataConversion.js
@@ -0,0 +1,28 @@
+let Score = 33
+let Section = "33q"
+console.log(Score)
+console.log(typeof(Score))
+
+//33 => "33"
+//"22q" => Nan
+
+let ChangedScore = String(Score)
+console.log(ChangedScore)
+console.log(typeof(ChangedScore))
+
+let ChangeSection = Number(Section)
+console.log(Section)
+console.log(typeof(Section))
+console.log(ChangeSection)
+console.log(typeof(ChangeSection))
+
+let LoggedIN =1
+let name = "Vishnu"
+let Changedlog = Boolean(LoggedIN)
+console.log(Changedlog)
+console.log(typeof(Changedlog))
+let Namec =Boolean(name)
+console.log(Namec)
+
+// "Vishnu"  = true
+//1 = true; 0 = false
