0b915c2ab3c770c6e569c895072da24362804168
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-05 20:18:38 +0000

    Comparison and conversion of null using operators

---
+++ 01_basics/04_Comparisions.js
@@ -0,0 +1,16 @@
+console.log(2 == 2)
+console.log(2>3)
+console.log(2!= 3)
+
+console.log(null == 0)
+console.log(null>2)
+console.log(null<2)
+
+//null is converted 0(zero) during comparisions operators like (<,>!=) for == null is not converted to 0
+
+console.log(undefined == 0)
+console.log(undefined > 2)
+console.log(undefined < 2)
+
+// === the comparision checks even Datatypes used in operators
+
