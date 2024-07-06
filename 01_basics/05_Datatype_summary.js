3d79101553b5064424490fa8a359e6caa26d8c1b
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-06 06:49:25 +0000

    Overall datatypes in Javascript

---
+++ 01_basics/05_Datatype_summary.js
@@ -0,0 +1,31 @@
+// Primitive
+// String,Number,BigInt,null,Undefined,Symbol,Boolean
+
+//Non premetive
+//Array,function,Object
+
+let a =22;
+let b = "22a";
+let c = true;
+let d = null;
+let e = undefined;
+let f = Symbol('123');
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
+// console.log(typeof(f))
+
+const heros = ["arjun","Nakul"]
+let hero = {hero:"Nakul"}
+let villan = function(){
+    console.log("Hello world")
+};
+
+console.log(typeof(heros))
+console.log(typeof(hero))
+console.log(typeof(villan))
+
+
