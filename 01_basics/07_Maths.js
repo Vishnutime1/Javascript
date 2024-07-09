e1646760f20c359846a55e2da03744997223feab
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-09 08:40:29 +0000

    Math functions

---
+++ 01_basics/07_Maths.js
@@ -0,0 +1,31 @@
+let score = 120
+console.log(score)
+
+let Player = new Number(12)
+console.log(typeof(Player))
+console.log(Player)
+
+console.log(Player.toString())
+console.log(Player.toString().indexOf(1))
+console.log(Player.toFixed(2))
+
+let money = new Number(1000000)
+console.log(money.toPrecision(6))
+console.log(money.toLocaleString("en-IN"))
+
+//==============Maths================
+
+Trail = Number(1200.73)
+console.log(Math.round(Trail))
+console.log(Math.ceil(Trail))
+console.log(Math.floor(Trail))
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+
+let min = 10
+let max = 20
+console.log(Math.random(Math.random()*(max - min+1)+ min))
+
+
+
