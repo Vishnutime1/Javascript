cd8502c8c0ffe23aa433cb2aefb8cbe3b5c6ce50
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-09 18:46:28 +0000

    More functions on arrays

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,22 @@
+let marvel_heros = 23
+let dc_heros = ["Bat man","Superman","Flash"]
+
+console.log(typeof marvel_heros)
+// // marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(typeof marvel_heros)
+// // all_heros = marvel_heros.concat(dc_heros)
+// // all_heros = [...marvel_heros,...dc_heros]
+// // console.log(all_heros)
+
+// // let my_arr =[1,23,[7,8,9,[10,11]]]
+// // console.log(my_arr)
+// // console.log(my_arr.flat(Infinity))
+console.log(Array.from({name : "Vishnu"}))  // interesting
+
+console.log(Array.isArray("Vishnu"))
+console.log(Array.from("Vishnu"))
+console.log(Array.of(marvel_heros))
+
+
+
