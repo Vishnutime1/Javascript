9a34e62c0721702d9f755acaee5945f43069f13a
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-09 17:13:38 +0000

    Array functions

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,35 @@
+// const arr = [0,1,2,3,4,5]
+// console.log(typeof(arr))
+
+const myarr = new Array(0,1,2,3,4,5)
+
+// console.log(typeof(myarr))
+
+// myarr.push("Marvel","Kraven")
+// console.log(myarr)
+// console.log(myarr[6][3])
+// myarr.pop()
+// console.log(myarr)
+// myarr.shift()
+// console.log(myarr)
+// myarr.unshift(500)
+// console.log(myarr)
+
+// console.log(myarr.includes(5))
+// console.log(myarr.indexOf(10))
+
+///Slice splice
+
+const nar = myarr.slice(1,3)
+console.log("A", nar)
+console.log("Orginal", myarr)
+
+const nar1 = myarr.splice(1,3)
+console.log("B", nar1)
+console.log("Orginal",myarr)
+
+
+
+
+
+
