8632d3a096777a495608d648010db36a6e5b1939
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-15 19:35:33 +0000

    Defining this and arrow usage

---
+++ 03_Basics/03_Arrows.js
@@ -0,0 +1,57 @@
+// const chai = {
+//     username : "Vishnu",
+//     Price : 1000000,
+
+//     WelcomeMessage : function() {
+//         console.log(`Hi ${this.username},is here `);
+//         console.log(this);
+//     }
+// }
+
+// chai.WelcomeMessage()
+// chai.username ="Reddy"
+// chai.WelcomeMessage()
+
+// console.log(this);//{} In browser points to windows
+
+// function chai() {
+//     username : "Vishnu",
+//     console.log(this);
+// }
+// chai()//In function many objects are present
+
+// const chai =  () => {
+//     username : "Vishnu",
+//     console.log(this)
+// }
+
+//  console.log(chai())// In arrow the this is empty object {}
+
+// let num1 =2
+// let num2 =3
+
+// const Numadd = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const Numadd = (num1, num2) => ( num1 + num2)
+// const Numadd = (num1, num2) => num1 + num2
+const Numadd = (num1, num2) => ( {username : "Vishnu"})
+
+
+console.log(Numadd(5,19))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
