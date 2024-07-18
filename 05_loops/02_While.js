a35cc671cb887b4c76bbc714fc82a468b7cf388a
Vishnutime1 <134081551+Vishnutime1@users.noreply.github.com>
2024-07-18 18:29:33 +0000

    do _while loops

---
+++ 05_loops/02_While.js
@@ -0,0 +1,25 @@
+ i=1
+
+ while (i<=10) {
+    console.log(`The number is ${i}`);
+    i++
+ }
+
+let myArray = ["flash","Batman","Superman"]
+j = myArray.length
+console.log(typeof(myArray.length));
+console.log(j);
+
+i=0
+
+while (i < myArray.length) {
+    console.log(myArray[i]);
+    i++
+}
+
+let score = 1
+
+do {
+    console.log(score);
+    score++
+} while (score <=10);
